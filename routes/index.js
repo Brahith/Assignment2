9c42389752c7b6875858a7ac66753a668fb75040
Brahith <brahithp@gmail.com>
2024-11-03 19:52:31 -0500

    Implementing EJS, Routing and Bootstrap
    Using Node.js, Express, Bootstrap, and Font Awesome, different page elements and styling are easily managed, demonstrating efficient routing and font-end dependency installation for scalable applications

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home'});
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home'});
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Me'});
+});
+
+/* GET products page. */
+router.get('/product', function(req, res, next) {
+  res.render('index', { title: 'Products'});
+});
+
+/* GET services page. */
+router.get('/service', function(req, res, next) {
+  res.render('index', { title: 'Service'});
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact Me'});
 });
 
 module.exports = router;
